2dd1e97a97bab797e362976e9b1c4c3079773891
Hirokazu Takahashi <taka@valinux.co.jp>
2012-11-12 09:55:09 +0900

    stop reusing a variable for different purposes

--- include/keymap.js
+++ include/keymap.js
@@ -1,25 +1,31 @@
 /*
  * noVNC: HTML5 VNC client
- * Copyright (C) 2012 Hirokazu Takahashi
- * Licensed under MPL 2.0 or any later version (see LICENSE.txt)
+ * Copyright (C) 2012 Joel Martin
+ * Licensed under LGPL-3 (see LICENSE.txt)
+ *
+ * See README.md for usage and integration instructions.
+ *
+ * TIGHT decoder portion:
+ * (c) 2012 Michael Tinglof, Joe Balaz, Les Piech (Mercuri.ca)
  */
 
-/*jslint browser: true, white: false, bitwise: false */
-/*global Util */
-
+"use strict";
+/*jslint white: false, browser: true, bitwise: false, plusplus: false */
+/*global Util*/
 
 var Kmap = {};
 Kmap.keymaps = {};
 
 Kmap.loadKeymap = function (kb) {
-    if (!kb || kb === 'default' || typeof Kmap.keymaps[kb] !== 'undefined')
+    if (!kb || kb === 'default' || typeof Kmap.keymaps[kb] !== 'undefined') {
         return;
+    }
     Kmap.keymaps[kb] = null;		// Now loading
     Util.load_scripts(["keymaps/" + kb + ".js"],
         function () {
-            var kb = this.src.split("/");
-            kb = kb[kb.length -1].split(".")[0];
-            var km = kb.replace(/[\-\+]/,"_");
+            var components = this.src.split("/"),
+                kb = components[components.length - 1].split(".")[0],
+                km = kb.replace(/[\-\+]/, "_");
             km = "Kmap.keymaps['" + kb + "'] = keymap_" + km;
             try {
                 eval(km);
